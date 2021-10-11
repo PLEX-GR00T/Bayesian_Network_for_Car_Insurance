e461b9b2c31bbb8f103b12cbb091e0f73e28287f
Priyank Thakkar <49235524+PLEX-GR00T@users.noreply.github.com>
2021-10-11 16:53:14 -0700

    Rstudio code and Correct Graph for .Lisp file

---
+++ Rstudio_Graph_generation.R
@@ -0,0 +1,20 @@
+library(bnlearn)
+library(tidyverse)
+
+carInsurance <- bnlearn::insurance
+carInsurance
+
+modelstring = paste0("[Age][Mileage][SocioEcon|Age][GoodStudent|Age:SocioEcon]",
+                     "[RiskAversion|Age:SocioEcon][OtherCar|SocioEcon][VehicleYear|SocioEcon:RiskAversion]",
+                     "[MakeModel|SocioEcon:RiskAversion][SeniorTrain|Age:RiskAversion]",
+                     "[HomeBase|SocioEcon:RiskAversion][AntiTheft|SocioEcon:RiskAversion]",
+                     "[RuggedAuto|VehicleYear:MakeModel][Antilock|VehicleYear:MakeModel]",
+                     "[DrivingSkill|Age:SeniorTrain][CarValue|VehicleYear:MakeModel:Mileage]",
+                     "[Airbag|VehicleYear:MakeModel][DrivQuality|RiskAversion:DrivingSkill]",
+                     "[Theft|CarValue:HomeBase:AntiTheft][Cushioning|RuggedAuto:Airbag]",
+                     "[DrivHist|RiskAversion:DrivingSkill][Accident|DrivQuality:Mileage:Antilock]",
+                     "[ThisCarDam|RuggedAuto:Accident][OtherCarCost|RuggedAuto:Accident]",
+                     "[MedCost|Age:Accident:Cushioning][ILiCost|Accident]",
+                     "[ThisCarCost|ThisCarDam:Theft:CarValue][PropCost|ThisCarCost:OtherCarCost]")
+dag = model2network(modelstring)
+graphviz.plot(dag, shape = "ellipse")
